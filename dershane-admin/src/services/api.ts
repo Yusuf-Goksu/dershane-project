ee020850fba7e38d9eefd948fedbcddbfaa0505c
Yusuf Göksu <yusufgks03@gmail.com>
2025-12-24 13:18:26 +0300

    debug: log api url

--- dershane-admin/src/services/api.ts
+++ dershane-admin/src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000/api", // Railway kullanacaksan sonra değiştiririz
+  baseURL: import.meta.env.VITE_API_URL,
+  //baseURL: "http://localhost:5000/api", // Railway kullanacaksan sonra değiştiririz
 });
 
 api.interceptors.request.use((config) => {
